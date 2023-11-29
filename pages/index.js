ac66b74554276a4e662f1995fd472e07109f43e2
anthony <anthonycakins@gmail.com>
2023-11-29 17:25:06 +1000

    added favicon & changed title

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,9 @@ export default function Home() {
   }, [isLoading]);
   return (
     <div>
+      <Head>
+      <title>Busy Bee</title>
+      </Head>
       {isLoading && <Loading />}
 
       <div
